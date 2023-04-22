f59beb486768dd63881442c9cb6702e2cf32e2e5
mirkobrignoli <127690671+mirkobrignoli@users.noreply.github.com>
2023-04-22 15:28:58 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,12 +16,12 @@ int main(){
 			cout << "0";
 		}
 		
-		else if (lista_nomi[1][0] == nome[0] && lista_nomi[1][1] == nome[1] && lista_nomi[1][2] == nome[2] && lista_nomi[1][3] == nome[3] && lista_nomi[1][4] == nome[4])
+		 if (lista_nomi[1][0] == nome[0] && lista_nomi[1][1] == nome[1] && lista_nomi[1][2] == nome[2] && lista_nomi[1][3] == nome[3] && lista_nomi[1][4] == nome[4])
 		{
 			cout << "1";
 		}
 		
-		else (lista_nomi[2][0] == nome[0] && lista_nomi[2][1] == nome[1] && lista_nomi[2][2] == nome[2] && lista_nomi[2][3] == nome[3] && lista_nomi[2][4] == nome[4])
+		 if (lista_nomi[2][0] == nome[0] && lista_nomi[2][1] == nome[1] && lista_nomi[2][2] == nome[2] && lista_nomi[2][3] == nome[3] && lista_nomi[2][4] == nome[4])
 		{
 			cout << "2";
 		}
